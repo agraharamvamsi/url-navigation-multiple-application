f86a55e13870c70b7dca3253a750ff5336ded40f
agraharamvamsi <agraharamvamsi@gmail.com>
2023-01-02 10:56:14 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def Tirupati(request):
+    return render(request,'Tirupati.html')
---
+++ app2/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def kalahasthi(request):
+    return render(request,'kalahasthi.html')
